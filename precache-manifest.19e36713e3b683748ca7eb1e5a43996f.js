6670fd5d4488775859432793cff3401a25a80a9a
smartsfa09 <smartsfa09@lottechilsung.co.kr>
2020-04-08 15:04:54 +0900

    Updates

--- precache-manifest.19e36713e3b683748ca7eb1e5a43996f.js
+++ precache-manifest.19e36713e3b683748ca7eb1e5a43996f.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "751d2e5986d3d9d8d89672ae96070c1d",
+    "revision": "268d3e218acf2aad72fba3e6d99ac86f",
     "url": "/movie_app/index.html"
   },
   {
-    "revision": "968aefc09e6ecb5fc97b",
-    "url": "/movie_app/static/css/main.48293c4f.chunk.css"
+    "revision": "36a82e87f2a9d8f532a7",
+    "url": "/movie_app/static/css/main.ed35ba28.chunk.css"
   },
   {
     "revision": "1d2d5261e5e33a966d0f",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "968aefc09e6ecb5fc97b",
+    "revision": "36a82e87f2a9d8f532a7",
     "url": "/movie_app/static/js/main.da37371e.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie_app/precache-manifest.6577e6248359ad86aae30a79b3734741.js"
+  "/movie_app/precache-manifest.19e36713e3b683748ca7eb1e5a43996f.js"
 );
 
 self.addEventListener('message', (event) => {
